a2dc02990b2468da25466c9d4776c1adc4ceefa3
jade <everydayfun@ukr.net>
2016-10-23 13:45:51 +0200

    working with 'studentList' class

--- src/UsageOOP/HomeworkOOP/StudentBasic.java
+++ src/UsageOOP/HomeworkOOP/StudentBasic.java
@@ -7,9 +7,11 @@ public class StudentBasic {
     public static void main(String[] args) {
         final StudentList stList = new StudentList();
 
-        stList.add(new Student("Rafal","Kochanwski",new Date(1990, 2,10)));
+        stList.add(new Student("Rafal","Kochanowski",new Date(1990, 2,10)));
         stList.add(new Student("Friend","Torwalds",new Date(1985, 5,4)));
-        stList.add(new Student("Daniel","Rocket",new Date(1995, 2,9)));
+        stList.add(new Student("Foxit","Reader",new Date(1995, 1,9)));
+        stList.add(new Student("Witcher","Thrzy",new Date(1993, 12,9)));
+        stList.add(new Student("Daniel","Rocket",new Date(1993, 9,9)));
 
         int searchName = stList.find("rafal");
         System.out.println("Search by name: "+
@@ -23,6 +25,6 @@ public class StudentBasic {
         System.out.println("Search by birth date: "+
                 stList.get(searchBDate).getSurname().toString()+" "+
                 stList.get(searchBDate).getName().toString());
-
+        stList.delStudent("Kochanowski");
     }
 }
--- src/UsageOOP/HomeworkOOP/StudentList.java
+++ src/UsageOOP/HomeworkOOP/StudentList.java
@@ -1,39 +1,56 @@
 package UsageOOP.HomeworkOOP;
 
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class StudentList {
-    private Student [] list = new Student[4];
+//    private Student [] list = new Student[4];
+    private ArrayList<Student> list = new ArrayList<>();
     private int counter=0;
 
     public void add(Student st){
-        list [counter++]=st;
+        list.add(counter++,st);
+        //list [counter++]=st;
     }
 
     public Student get(int number){
-        return list[number];
+        return list.get(number);
+//        return list[number];
     }
 
     //searches for the student by name
     public int find(String name){
         for (int i = 0; i < counter; i++) {
-            if(list[i].getName().equalsIgnoreCase(name))
+//            if(list[i].getName().equalsIgnoreCase(name))
+              if(list.get(i).getName().equalsIgnoreCase(name))
                 return i;
         } return -1;
     }
 
     public int findSurname(String surname){
         for (int i = 0; i < counter; i++) {
-            if(list[i].getSurname().equalsIgnoreCase(surname))
+//            if(list[i].getSurname().equalsIgnoreCase(surname))
+            if(list.get(i).getSurname().equalsIgnoreCase(surname))
                 return i;
         } return -1;
     }
 
     public int findBirthday(Date birthday){
         for (int i = 0; i < counter; i++) {
-            if(list[i].getBirthday().equals(birthday))
+//            if(list[i].getBirthday().equals(birthday))
+            if(list.get(i).getBirthday().equals(birthday))
                 return i;
         } return -1;
+    }
+
+    public void delStudent(String surname){
+        for (int i = 0; i < counter; i++) {
+            if(list.get(i).getSurname().equalsIgnoreCase(surname)){
+                //int count=+i;
+                list.remove(i);
+                break;
+
+            }
+        }System.out.println("There is no student with such surname.");
     }
 }
