9584f6ad07e5e9aa2b39102e8698ddd8b07cbfb9
jade <everydayfun@ukr.net>
2016-12-13 20:46:41 +0100

    program searches for the files at the directory with few listed extensions

--- src/InOutJava/example/FindFiles/Main.java
+++ src/InOutJava/example/FindFiles/Main.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 
 public class Main {
 
+//program should find all the files at the directory with listed extensions
+
 	static class MyFileFilter implements FilenameFilter {
 		private String ext;
 
@@ -19,25 +21,35 @@ public class Main {
 		}
 	}
 
-	private static void findFiles(String srcPath, String ext, ArrayList<String> list) throws IOException {
+	private static void findFiles(String srcPath, ArrayList <String> ext, ArrayList<String> list) throws IOException {
 		File dir = new File(srcPath);
-		File[] files = dir.listFiles(new MyFileFilter(ext));
+		ArrayList <File[]> files = new ArrayList<>();
 
-		for (int i = 0; i < files.length; i++) {
-			list.add(srcPath + files[i].getName());
+		for(String s : ext){
+			files.add(dir.listFiles(new MyFileFilter(s)));
 		}
+
+		for(File [] f : files){
+			for (int i = 0; i < f.length; i++) {
+				list.add(srcPath + f[i].getName());
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
+
+		ArrayList<String> fileNameList = new ArrayList<>();
+		//double brace initializer
+		ArrayList<String> extensionsList = new ArrayList() {{add(".txt"); add(".iso");}};
 
 		try {
-			findFiles("d:\\", ".txt", list);
+			findFiles("d:\\", extensionsList, fileNameList);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		for (String s : list)
+		for (String s : fileNameList)
 			System.out.println(s);
 	}
 
