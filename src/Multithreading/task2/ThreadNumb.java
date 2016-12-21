7c526f878eebb95449e3f21a097f4f836eb18fa2
jade <everydayfun@ukr.net>
2016-12-21 19:45:50 +0100

    task2 - class creates 100 threads, each of them prints his number and then waits till interrupting

--- src/Multithreading/task2/ThreadNumb.java
+++ src/Multithreading/task2/ThreadNumb.java
@@ -1,4 +1,15 @@
 package Multithreading.task2;
 
-public class ThreadNumb {
+public class ThreadNumb extends Thread{
+    public void run(){
+        System.out.println("Thats the thread numb: "+ getId()+" is started!");
+//        try {
+//            join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        if(isInterrupted()){
+//            System.out.println("interrupted");
+//        }
+    }
 }
--- src/Multithreading/task2/ThreadNumbTest.java
+++ src/Multithreading/task2/ThreadNumbTest.java
@@ -1,4 +1,25 @@
 package Multithreading.task2;
 
+//class that creates 100 threads, each of
+//them should print his number and then wait for interrupting
+
+import java.util.ArrayList;
+
 public class ThreadNumbTest {
+    public static void main(String[] args) {
+        ArrayList<ThreadNumb> l = new ArrayList();
+        try {
+        for (int i = 0; i < 100 ; i++) {
+            ThreadNumb t = new ThreadNumb();
+            t.start();
+            l.add(t);
+        }
+        for(ThreadNumb t : l){
+            t.interrupt();
+        }
+        Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
