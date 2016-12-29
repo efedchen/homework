dded36057f728524d0a8ea37d39bf40a979f1603
jade <everydayfun@ukr.net>
2016-12-29 19:19:32 +0100

    updated task6 accountSync class

--- src/Multithreading/task6/AccountSync.java
+++ src/Multithreading/task6/AccountSync.java
@@ -1,4 +1,30 @@
 package Multithreading.task6;
 
 public class AccountSync {
+    public static void main(String[] args) {
+        Account acc = new Account(1000);
+        Transaction[] transactions = {
+                new Transaction(acc, 100),
+                new Transaction(acc, 500),
+                new Transaction(acc, 200),
+                new Transaction(acc, 50),
+                new Transaction(acc, 150)
+        };
+
+        // transaction starts
+        for (Transaction t : transactions)
+            t.start();
+
+        // waiting till end of transaction
+        for (Transaction t : transactions) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //if the withdraw is 'synchronized' it wont work
+        System.out.println("Total: " + acc.get());
+    }
 }
