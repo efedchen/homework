23297aa79ef30e9b4546342096bcef71e5142669
jade <everydayfun@ukr.net>
2017-01-17 20:03:22 +0100

    some activity with lists

--- src/ArraysNcollections/WareHouse/WareHouse.java
+++ src/ArraysNcollections/WareHouse/WareHouse.java
@@ -1,4 +1,18 @@
 package ArraysNcollections.WareHouse;
 
-public class WareHouse {
+public class WareHouse<E> {
+    private E[] data;
+
+    public E[] getData(){
+        return data;
+    }
+    public void setData(E[] data){
+        this.data = data;
+    }
+    public E get(int index){
+        return data[index];
+    }
+    public int getSize(){
+        return data.length;
+    }
 }
